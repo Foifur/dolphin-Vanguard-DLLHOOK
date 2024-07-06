2e5ac428d5303538121b51a07175889b79036e2c
Foifur <jqgreenan@gmail.com>
2024-07-06 14:34:42 -0700

    Initial publish
    keeping all of the old Vanguard implementation so I can compare later to DLLHOOK changes

---
+++ Source/Core/Common/Config/Enums.h
@@ -0,0 +1,49 @@
+// Copyright 2017 Dolphin Emulator Project
+// Licensed under GPLv2+
+// Refer to the license.txt file included.
+
+#pragma once
+
+#include <array>
+
+namespace Config
+{
+enum class LayerType
+{
+  Base,
+  GlobalGame,
+  LocalGame,
+  Movie,
+  Netplay,
+  CommandLine,
+  CurrentRun,
+  //Narrysmod - Add vanguard
+  Vanguard,
+  Meta,
+};
+
+enum class System
+{
+  Main,
+  SYSCONF,
+  GCPad,
+  WiiPad,
+  GCKeyboard,
+  GFX,
+  Logger,
+  Debugger,
+  DualShockUDPClient,
+};
+
+constexpr std::array<LayerType, 8> SEARCH_ORDER{{
+  //Narrysmod - Add vanguard
+    LayerType::Vanguard,
+    LayerType::CurrentRun,
+    LayerType::CommandLine,
+    LayerType::Movie,
+    LayerType::Netplay,
+    LayerType::LocalGame,
+    LayerType::GlobalGame,
+    LayerType::Base,
+}};
+}  // namespace Config
