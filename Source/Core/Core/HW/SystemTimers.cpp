d802477e68e11b9d7ad50daf76dd893de69a8f09
Foifur <jqgreenan@gmail.com>
2024-07-08 20:48:33 -0700

    reverted changes to CORESTEP function as it needs to keep the dll loaded

--- Source/Core/Core/HW/SystemTimers.cpp
+++ Source/Core/Core/HW/SystemTimers.cpp
@@ -68,7 +68,6 @@ IPC_HLE_PERIOD: For the Wii Remote this is the call schedule:
 #include "Core/PowerPC/PowerPC.h"
 #include "VideoCommon/Fifo.h"
 
-#include "DolphinQt/VanguardHelpers.h" //RTC_Hijack
 
 namespace SystemTimers
 {
@@ -157,7 +156,12 @@ void PatchEngineCallback(u64 userdata, s64 cycles_late)
   if (PatchEngine::ApplyFramePatches())
   {
     // RTC_Hijack: call Vanguard function
-    CallImportedFunction("CORESTEP");
+    // since this function is getting called frequently we can't load and free the dll constantly,
+    // so this function cannot use the helper function.
+    HINSTANCE vanguard = LoadLibraryA("DolphinVanguard-Hook.dll");
+    typedef void (*CORESTEP)();
+    CORESTEP CoreStep = (CORESTEP)GetProcAddress(vanguard, "CORESTEP");
+    CoreStep();
     next_schedule = vi_interval - cycles_pruned;
     cycles_pruned = 0;
   }
